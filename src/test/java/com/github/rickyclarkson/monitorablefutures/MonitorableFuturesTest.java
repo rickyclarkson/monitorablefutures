2b16ba6340c2ac4f44bf231004f218b536dd6dc6
Ricky Clarkson <ricky.clarkson@gmail.com>
2012-08-05 21:55:13 -0300

    Seeing whether a sleep(500) fixes a failure.  I'll find a better way later.

--- src/test/java/com/github/rickyclarkson/monitorablefutures/MonitorableFuturesTest.java
+++ src/test/java/com/github/rickyclarkson/monitorablefutures/MonitorableFuturesTest.java
@@ -37,6 +37,7 @@ public class MonitorableFuturesTest {
                 continue;
 
             if (integer == 2) {
+                Thread.sleep(500);
                 assertTrue(future.isDone());
                 return;
             }
